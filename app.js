7c5e3c18d0aceb486100e380b2f3714eb3766bca
Steve Bachtel <sbachtel@gmail.com>
2016-05-12 19:34:56 -0700

    made an gitignore

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
+var allProducts = [];
+var productNames = ['bag', 'banana', 'boots', 'chair', 'cthulhu', 'dragon', 'pen', 'scissors', 'shark', 'sweep', 'unicorn', 'usb', 'water_can', 'wine_glass'];
 
+function Product(name, path){
+  this.name = name;
+  this. = path;
+
+  this.checkImages = function(){
+    return this.name + this.images;
+  }
+}
