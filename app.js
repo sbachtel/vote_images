26938da8a4ecde3304b33a3521524e8f13cf2d37
Steve Bachtel <sbachtel@gmail.com>
2016-05-14 15:29:44 -0700

    displaying random images

--- app.js
+++ app.js
@@ -14,36 +14,61 @@ function Product(name, path) {//constructor
   }
  })();
 
+var productRank = { //<- object literal
+  totalClicks: 0,//assign object to null except total clicks
+  leftObj: null, //obj left, right, mid: null
+  midObj: null,
+  rightObj: null,
+  leftEls: document.getElementById('img1'),
+  midEls: document.getElementById('img2'),
+  rightEls: document.getElementById('img3'),
+  imageEls: document.getElementById('images'),
 
-   var productRank = { //<- object literal
-     //assign object to null except total clicks
-   //total clicks
-   //obj left, right, mid: null
     // TODO: All the properties of the object! What do you think you need? Try to write one piece at a time and make sure it does what you want before writing a little more.
     // NOTE: A-C-P reminder... Make very intentional and iterative changes to your code, and then A-C-P.
 
-     getRandomIndex: function() {
-       return Math.floor(Math.random() * productNames.length);
+  getRandomIndex: function() {
+    return Math.floor(Math.random() * productNames.length);
        // TODO: Hmm... what's going to happen here?
-     },
+  },
 
-    displayImages: function() {
+  displayImages: function() {
+    productRank.leftObj = allProducts[productRank.getRandomIndex()];
+    productRank.midObj = allProducts[productRank.getRandomIndex()];
+    productRank.rightObj = allProducts[productRank.getRandomIndex()];
+
+      if(productRank.leftObj === productRank.midObj || productRank.leftObj === productRank.rightObj || productRank.rightObj === productRank.midObj){
+      productRank.displayImages();
+    }
+    productRank.leftEls.src = productRank.leftObj.path;
+    productRank.leftEls.id = productRank.leftObj.name;
+
+    productRank.midEls.src = productRank.midObj.path;
+    productRank.midEls.id = productRank.midObj.name;
+
+    productRank.rightEls.src = productRank.rightObj.path;
+    productRank.rightEls.id = productRank.rightObj.name;
       // TODO: Hmm... what's going to happen here?
       //product rank object put all procduts and get random into the product
       //make an if statement to make sure they are not the same images.
 
       //start pluging in obj elem into html left, right, center elemnts
       //need path(scr) and name (id)
-    },
+  },
 
-    tallyClicks: function(elementId) {
+  tallyClicks: function(elementId) {
+    for (var i in allProducts) {
+      if (object.hasOwnProperty(i)) {
+
+      }
+    }
       // TODO: Hmm... what's going to happen here?
 
       //when call tallyClicks you need and id of an elementId
       //need for loop and will refrence productRank
-    },
+  },
 
-    displayResults: function() {
+  displayResults: function() {
       // TODO: Hmm... what's going to happen here?
   //create ul list
   //create for loop of allProducts
@@ -51,13 +76,13 @@ function Product(name, path) {//constructor
   // a var which will add allProducts array and .name to 'has' allProducts array .tally +'voteImages'
   //then make new str with charset and slice
   //appent the str to the li
-    },
+  },
 
-    showButton: function() {
+  showButton: function() {
       // TODO: Hmm... what's going to happen here?
-    },
+  },
 
-    onClick: function() {
+  onClick: function() {
       // TODO: Hmm... what's going to happen here?
       //if statement evnt targ id of the productRank leftobj.name or rightobj or centerobj
       //productRank tallyclikc event target and id
@@ -65,8 +90,8 @@ function Product(name, path) {//constructor
       //remove eventlisterner(cick,productRank.onClick);
       //products displayImages()
       //else with about warning click an image
-    }
-  };
+  }
+};
 
 // productRank.imageEls.addEventListener('click', productRank.onClick);
  productRank.displayImages();
